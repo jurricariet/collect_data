c20d68723fd4fd407f55a8d37a89b25a1365d07a
jurricariet <juanurricariet@gmail.com>
2024-04-29 21:49:53 -0300

    graph subtitle

--- scripts/bcra.R
+++ scripts/bcra.R
@@ -6,10 +6,13 @@ library(jsonlite)
 variables <- GET(glue::glue('https://api.bcra.gob.ar/estadisticas/v1/PrincipalesVariables'))
 lista_variables <- as.data.frame(fromJSON(rawToChar(variables$content)))
 
+id_variable <- 1
+fecha_desde <- '1990-01-01'
+fecha_hasta <- Sys.Date()
 
-reservas <- GET(glue::glue('https://api.bcra.gob.ar/estadisticas/v1/DatosVariable/1/1990-01-01/{Sys.Date()}'))
+data_api <- GET(glue::glue('https://api.bcra.gob.ar/estadisticas/v1/DatosVariable/{id_variable}/{fecha_desde}/{Sys.Date()}'))
 
-data = as.data.frame(fromJSON(rawToChar(reservas$content))$results) %>% 
+data <-  as.data.frame(fromJSON(rawToChar(data_api$content))$results) %>% 
   mutate(fecha = lubridate::dmy(fecha),
          valor = as.numeric(valor))
 
@@ -18,5 +21,5 @@ ggplot(data,aes(x=fecha,y=valor,group=1))+
   geom_line()+
   scale_y_continuous()+
   labs(x='',y='',title='Reservas Internacionales del BCRA',
-       subtitle='En millones de dólares - cifras provisorias sujetas a cambio de valuación')+
+       subtitle='En miles de millones de dólares - cifras provisorias sujetas a cambio de valuación')+
   theme_minimal()
